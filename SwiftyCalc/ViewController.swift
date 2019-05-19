7f873095b509fb20543ec982dc2f5ce0b8ba5b22
VladimirBrejcha <python2python@icloud.com>
2019-05-19 18:18:03 +0300

    Initial Commit

---
+++ SwiftyCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyCalc
+//
+//  Created by Vladimir Korolev on 19/05/2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
